1b79e07db3414a837d98fad4d76fa28df073d721
Abhigyan Bhattacharya <applicationreaperhotel3256@gmail.com>
2026-05-19 20:43:49 +0530

    style: change footer copyright text and border color to #030303

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -64,7 +64,7 @@ export function Footer() {
           </div>
         </div>
 
-        <div className="border-t border-black/10 mt-8 md:mt-12 pt-4 text-center text-black/60 text-[10px]">
+        <div className="border-t border-[#030303] mt-8 md:mt-12 pt-4 text-center text-[#030303] text-[10px]">
           <p>&copy; {new Date().getFullYear()} M R EXIM. All rights reserved.</p>
         </div>
       </div>
